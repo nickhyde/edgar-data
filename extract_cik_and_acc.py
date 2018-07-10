7242126e9b9927b174bf4e4345d4fbda5b12241c
Nick Hyde <nickhyde314@yahoo.com>
2018-07-09 20:42:30 -0700

    improved python extraction file

--- extract_cik_and_acc.py
+++ extract_cik_and_acc.py
@@ -1,6 +1,22 @@
-fin  = open('1994-QTR1.tsv','r')
-fout = open('1994-QTR1.csv','w+')
-for line in fin.readlines():
-  line1 = line.split('|')
-  line2 = '|'.join([line1[0],line1[2],line1[3],line1[4][12+len(line1[0]):len(line1[4]) - 4]])
-  fout.write(line2+'\n')
+import glob
+if __name__ == '__main__':
+  print ("Starting ...\n")
+  for finame in glob.glob('./*.tsv'):
+    glob.sys.stdout.write("Working on file [%s]" % finame)
+    line_count = 0
+    with open(finame ,'r') as fhin:
+       foname =finame[:-4]+'.csv'
+       if glob.os.path.isfile(foname):
+            print("\nfile %s already exists. Aborting." % foname)
+            glob.sys.exit(1)
+       with open(foname,'w+') as fhout:
+         for line in fhin.readlines():
+           line_count += 1
+           if line_count % 10000 == 0: glob.sys.stdout.write('.')
+           words = line.split('|')
+           if words[4][0:11] != words[5][0:11] or words[5][0:11] != "edgar/data/":
+              print "\nedgar/data/ missing in word[4] %s or word[5] %s. Fatal error. Terminating" % (words[4],words[5])
+              glob.sys.exit(1)
+           fhout.write('|'.join([words[0],words[2],words[3],words[4][12+len(words[0]):len(words[4]) - 4]]) + '\n')
+    glob.sys.stdout.write('.line count %d\nConverted file to [%s]\n' % (line_count, foname))
+  print("Finished converting files.")
