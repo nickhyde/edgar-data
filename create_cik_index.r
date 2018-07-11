943289192e2cc0aee95b778899574d8793c4e1df
Larry-Hignight <larry.hignight@gmail.com>
2018-07-11 00:20:02 -0700

    Fixed a bug in the CIK index (which was _obviously_ due to Nick's distractions... LOL).

--- create_cik_index.r
+++ create_cik_index.r
@@ -1,16 +1,21 @@
 ## This script creates the CIK index file using the tsv files
-setwd('/home/larry/Github-Public/python-edgar/data/')
+## Note: The CIK numbers are not padded with leading zeroes as the EDGAR URLs use non-padded values
 
-filenames <- list.files('.', pattern = '*.tsv')
-for (filename in filenames) {
+process_index_file <- function(filename) {
   print(sprintf("Processing file: %s", filename))
-  write_index_file(filename)
+  x <- read.csv(filename, header = F, sep = '|', colClasses = c('integer', rep('character', 5)))[ , 1:2]
+  names(x) <- c('CIK', 'Corporation')
+  unique(x)
 }
 
-write_index_file <- function(filename) {
-  x <- read.csv(filename, header = F, sep = '|')
-  names(x)[1:2] <- c('CIK', 'Corporation')
-  x2 <- unique(x[ , 1:2])
-  x2 <- x2[order(x2$CIK), ]
-  write.csv(x2, paste0('../cik-index-', filename), row.names = F)
-}
+# Process the tsv files
+setwd('~/Github-Public/python-edgar/data/')
+filenames <- list.files('.', pattern = '*.tsv')
+x <- do.call(rbind, lapply(filenames, process_index_file))
+x <- unique(x)
+x <- x[order(x$CIK), ]
+head(x)
+
+# Write the CIK index to disk
+setwd('../../edgar-data/index-data/')
+write.csv(x, file = 'cik-index.csv', row.names = FALSE)
